882889848acbff94b5832f8577832211f6c7a041
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-22 10:02:24 -0300

    Limpieza de comentarios

--- src/main/java/com/opticamarcosweb/model/Lente.java
+++ src/main/java/com/opticamarcosweb/model/Lente.java
@@ -3,17 +3,7 @@ package com.opticamarcosweb.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -41,31 +31,12 @@ public class Lente {
 	private Boolean esBifocal;
 	@Column(nullable = false, name="es_multifocal")
 	private Boolean esMultifocal;
-	
-	@ManyToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name="id_ficha")
-	private Ficha ficha;
-	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy="lente", orphanRemoval=true,fetch = FetchType.LAZY)
-	private Set<MedidaLente> medidasLentes = new HashSet<MedidaLente>();
-	
+
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private Set<Armazon> armazones = new HashSet<Armazon>();
-	
-//	public void addMedidasLentes(MedidaLente medidaLente) {
-//		medidasLentes.add(medidaLente);
-//	}
-//
-//	public void removeMedidasLentes(MedidaLente medidaLente) {
-//		medidasLentes.remove(medidaLente);
-//	}
-//
-//	public void addArmazones(Armazon armazon) {
-//		armazones.add(armazon);
-//	}
-//
-//	public void removeArmazones(Armazon armazon) {
-//		armazones.remove(armazon);
-//	}
-		
+	private Set<MedidaLente> medidasLentes = new HashSet<MedidaLente>();
+
+	@OneToOne
+	private Armazon armazon;
+
+
 }
