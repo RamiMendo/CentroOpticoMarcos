3b955c881d1946dc50a087460c03ba4323104a49
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-22 10:05:27 -0300

    Cambio de notacion para el tipo de relacion con Cliente

--- src/main/java/com/opticamarcosweb/model/Ficha.java
+++ src/main/java/com/opticamarcosweb/model/Ficha.java
@@ -4,15 +4,7 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -32,35 +24,24 @@ public class Ficha {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id_ficha")
 	private Long idFicha;
+
 	@Column(nullable = false)
 	private LocalDate fecha;
+
 	@Column(nullable = false)
 	private Double senia;
+
 	@Column(nullable = false)
 	private Double saldo;
+
 	@Column(nullable = false)
 	private Double total;
 	
+	@OneToOne
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
+
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-	private Set<Cliente> clientes = new HashSet<Cliente>();
-	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy="ficha", orphanRemoval=true, fetch = FetchType.LAZY)
 	private Set<Lente> lentes = new HashSet<Lente>();
-	
-//	public void addCliente(Cliente cliente) {
-//		clientes.add(cliente);
-//	}
-//
-//	public void removeCliente(Cliente cliente) {
-//		clientes.remove(cliente);
-//	}
-//
-//	public void addLente(Lente lente) {
-//		lentes.add(lente);
-//	}
-//
-//	public void removeLente(Lente lente) {
-//		lentes.remove(lente);
-//	}
-	
+
 }
