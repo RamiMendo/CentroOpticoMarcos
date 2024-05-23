f6e1bbc587685034c229a6155b9a7807040d7cbd
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-23 17:59:23 -0300

    Agrega el metodo para la busqueda de fichas por cliente en el servicio

--- src/main/java/com/opticamarcosweb/service/FichaService.java
+++ src/main/java/com/opticamarcosweb/service/FichaService.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
+import com.opticamarcosweb.model.Cliente;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -49,26 +49,9 @@ public class FichaService {
 		List<Ficha> listaFichas = new ArrayList<Ficha>();
 		listaFichas = fichaRepository.getAllFichasByFiltro(fechaDesde, fechaHasta, totalDesde, totalHasta);
 		return listaFichas;
-//		if(estaPagado){
-//			listaFichas = listaFichas
-//					.stream()
-//					.filter((f) -> f.getSaldo().equals(0)).collect(Collectors.toList());
-//		}else{
-//			listaFichas = listaFichas
-//					.stream()
-//					.filter((f) -> !f.getSaldo().equals(0)).collect(Collectors.toList());
-//		}
-//
-//		if(estaSeniado){
-//			listaFichas = listaFichas
-//					.stream()
-//					.filter((f) -> f.getSenia().equals(0)).collect(Collectors.toList());
-//		}else{
-//			listaFichas = listaFichas
-//					.stream()
-//					.filter((f) -> !f.getSenia().equals(0)).collect(Collectors.toList());
-//		}
+	}
 
-
+	public List<Ficha> getAllFichasByidCliente(Integer idCliente){
+		return fichaRepository.findAllByidCliente(idCliente);
 	}
 }
