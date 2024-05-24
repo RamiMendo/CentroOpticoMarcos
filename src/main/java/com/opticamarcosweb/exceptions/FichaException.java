9d4f0839f442c2cb01e71ea763632a1440020065
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-24 15:23:13 -0300

    Modifica metodo contructor de la clase fichaexception

--- src/main/java/com/opticamarcosweb/exceptions/FichaException.java
+++ src/main/java/com/opticamarcosweb/exceptions/FichaException.java
@@ -1,15 +1,17 @@
 package com.opticamarcosweb.exceptions;
 
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+@Data
 public class FichaException extends Exception{
 
     private String message;
     private HttpStatus httpStatus;
 
-    public FichaException(String message, String message1, HttpStatus httpStatus) {
+    public FichaException(String message, HttpStatus httpStatus) {
         super(message);
-        this.message = message1;
+        this.message = message;
         this.httpStatus = httpStatus;
     }
 }
