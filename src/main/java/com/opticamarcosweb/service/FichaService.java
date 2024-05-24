7531112a93a1d527a638298f7b474c421e605058
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-24 15:22:07 -0300

    Agrega validacion de id el metodo updateSaldoCero del Servicio

--- src/main/java/com/opticamarcosweb/service/FichaService.java
+++ src/main/java/com/opticamarcosweb/service/FichaService.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import com.opticamarcosweb.model.Cliente;
+import com.opticamarcosweb.exceptions.FichaException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.opticamarcosweb.model.Ficha;
@@ -41,7 +42,12 @@ public class FichaService {
 	}
 
     @Transactional
-	public void updateSaldoCero(Integer id){
+	public void updateSaldoCero(Integer id) throws FichaException {
+		Optional<Ficha> ficha = fichaRepository.findById(id);
+
+		if(ficha.isEmpty()) {
+			throw new FichaException("Ficha no existe!", HttpStatus.NOT_FOUND);
+		}
 		fichaRepository.updateSaldoCero(id);
 	}
 
