85d2b2e92fc3ef372c1bffcfe6212a16fc9daa32
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-25 11:47:52 -0300

    Modificacion de notaciones en el controllador de armazones

--- src/main/java/com/opticamarcosweb/controller/ArmazonController.java
+++ src/main/java/com/opticamarcosweb/controller/ArmazonController.java
@@ -1,21 +1,14 @@
 package com.opticamarcosweb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.opticamarcosweb.model.Armazon;
 import com.opticamarcosweb.service.ArmazonService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,31 +20,21 @@ public class ArmazonController {
 	@Autowired
 	private ArmazonService armazonService;
 
-	@GetMapping(value="/")
+	@GetMapping
 	@Operation(summary="Devuelve un listado de Armazones", description="No tiene parámetros de entrada, no está paginada y devuelve todos los armazones de bd", tags= {"Armazones"})
-	@ApiResponses(value= {
-			@ApiResponse(responseCode="200", description = "Se encontraron armazones en bd!"),
-			@ApiResponse(responseCode="404", description = "No se encotraron armazones en BD!")
-			
-	})
-	public List<Armazon> getListaArmazones() {
+	public @ResponseBody List<Armazon> getListaArmazones() {
 		return armazonService.getAllArmazones();
 	}
 	
 	@GetMapping(value="/{id}")
 	@Operation(summary="Devuelve un armazon apartir de ID ingresado", description="Tiene parámetros de entrada, está paginada y devuelve el armazon a partir del ID ingresado", tags= {"Armazones"})
-	@ApiResponses(value= {
-			@ApiResponse(responseCode="200", description = "Se encontraron armazones en bd!"),
-			@ApiResponse(responseCode="404", description = "No se encotraron armazones en BD!")
-			
-	})
 	private @ResponseBody Optional<Armazon> getArmazonById(@PathVariable("id") Integer id) {
-		return  armazonService.getArmazonById(id);
+		return armazonService.getArmazonById(id);
 	}
 	
-	@PostMapping(value="/",produces="application/json")
+	@PostMapping(consumes="application/json")
 	@Operation(summary="Agrega un armazon", description="Debe enviar el objeto armazon a ingresar", tags= {"Armazones"})
-	private @ResponseBody Armazon addArmazon(Armazon armazon) {	
+	private @ResponseBody Armazon addArmazon(@RequestBody Armazon armazon) {
 		return armazonService.addArmazon(armazon);
 	}
 	
