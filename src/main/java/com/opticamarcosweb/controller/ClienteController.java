b41c6004282d5c7ebf844cc580e88ea86eed9ae5
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-25 13:07:46 -0300

    Modificacion de notaciones en el controlador de clientes

--- src/main/java/com/opticamarcosweb/controller/ClienteController.java
+++ src/main/java/com/opticamarcosweb/controller/ClienteController.java
@@ -4,14 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.opticamarcosweb.model.Cliente;
 import com.opticamarcosweb.service.ClienteService;
@@ -27,31 +20,21 @@ public class ClienteController {
 	@Autowired
 	private ClienteService clienteService;
 	
-	@GetMapping(value="/")
+	@GetMapping
 	@Operation(summary="Devuelve un listado de Clientes", description="No tiene parámetros de entrada, no está paginada y devuelve todos los clientes de bd", tags= {"Clientes"})
-	@ApiResponses(value= {
-			@ApiResponse(responseCode="200", description = "Se encontraron clientes en bd!"),
-			@ApiResponse(responseCode="404", description = "No se encotraron clientes en BD!")
-			
-	})
 	private @ResponseBody List<Cliente> getListaClientes() {
 		return clienteService.getListaCliente();
 	}
 		
-	@GetMapping(value="/{id}")
-	@Operation(summary="Devuelve un cliente apartir de ID ingresado", description="Tiene parámetros de entrada, está paginada y devuelve el cliente a partir del ID ingresado", tags= {"Clientes"})
-	@ApiResponses(value= {
-			@ApiResponse(responseCode="200", description = "Se encontraron clientes en bd!"),
-			@ApiResponse(responseCode="404", description = "No se encotraron clientes en BD!")
-			
-	})
+	@GetMapping(path="/{id}")
+	@Operation(summary="Devuelve un cliente apartir de ID ingresado", description="Tiene parámetros de entrada, devuelve el cliente a partir del ID ingresado", tags= {"Clientes"})
 	private @ResponseBody Optional<Cliente> getClienteByID(@PathVariable("id") Integer id) {
-		return  clienteService.getClienteById(id);
+		return clienteService.getClienteById(id);
 	}
 	
-	@PostMapping(value="/",produces="application/json")
+	@PostMapping(consumes="application/json")
 	@Operation(summary="Agrega un cliente", description="Debe enviar el objeto cliente a ingresar", tags= {"Clientes"})
-	private @ResponseBody Cliente addCliente(Cliente cliente) {	
+	private @ResponseBody Cliente addCliente(@RequestBody Cliente cliente) {
 		return clienteService.addCliente(cliente);
 	}
 	
@@ -61,9 +44,9 @@ public class ClienteController {
 		clienteService.deleteCliente(id);
 	}
 	
-	@PutMapping(path="/", produces="application/json")
+	@PutMapping(consumes="application/json")
 	@Operation(summary="Permite modificar un cliente", description="Debe enviar el objeto a modificar, no se puede modificar el id", tags= {"Clientes"})
-	public @ResponseBody Cliente updateCliente(Cliente cliente) {
+	public @ResponseBody Cliente updateCliente(@RequestBody Cliente cliente) {
 		return clienteService.updateCliente(cliente);
 	}
 	
