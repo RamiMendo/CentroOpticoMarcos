950f859ffe4a445fcb7996886e0e8648bc05843d
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-27 09:50:35 -0300

    Limpieza de codigo y cambio de notaciones en el controller de fichas

--- src/main/java/com/opticamarcosweb/config/SeguridadConfig.java
+++
@@ -1,8 +0,0 @@
-package com.opticamarcosweb.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-
-public class SeguridadConfig {
-}
--- src/main/java/com/opticamarcosweb/controller/FichaController.java
+++ src/main/java/com/opticamarcosweb/controller/FichaController.java
@@ -1,12 +1,9 @@
 package com.opticamarcosweb.controller;
 
 import com.opticamarcosweb.exceptions.FichaException;
-import com.opticamarcosweb.model.Cliente;
 import com.opticamarcosweb.model.Ficha;
 import com.opticamarcosweb.service.FichaService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -23,24 +20,14 @@ public class FichaController {
     @Autowired
     private FichaService fichaService;
 
-    @GetMapping(value="/")
+    @GetMapping
     @Operation(summary="Devuelve un listado de Fichas", description="No tiene parámetros de entrada, no está paginada y devuelve todos las fichas de bd", tags= {"Fichas"})
-    @ApiResponses(value= {
-            @ApiResponse(responseCode="200", description = "Se encontraron fichas en bd!"),
-            @ApiResponse(responseCode="404", description = "No se encotraron fichas en BD!")
-
-    })
     private @ResponseBody List<Ficha> getListaFichas() {
         return fichaService.getAllFichas();
     }
 
     @GetMapping(value="/{id}")
     @Operation(summary="Devuelve una ficha apartir de ID ingresado", description="Tiene parámetros de entrada y devuelve la ficha a partir del ID ingresado", tags= {"Fichas"})
-    @ApiResponses(value= {
-            @ApiResponse(responseCode="200", description = "Se encontraron fichas en bd!"),
-            @ApiResponse(responseCode="404", description = "No se encotraron fichas en BD!")
-
-    })
     private @ResponseBody Optional<Ficha> getFichaById(@PathVariable("id") Integer id) {
         return  fichaService.getFichaById(id);
     }
@@ -51,13 +38,13 @@ public class FichaController {
         return fichaService.getAllFichasByFiltro(fechaDesde, fechaHasta, totalDesde, totalHasta, estaSeniado, estaPagado);
     }
 
-    @GetMapping(path = "/cliente/", produces = "application/json")
+    @GetMapping(path = "/cliente", produces = "application/json")
     @Operation(summary = "Busca todas las fichas a partir del id del cliente", description = "Debe enviar id del cliente", tags = {"Fichas"})
     private @ResponseBody List<Ficha> getAllFichasByCliente(@RequestParam Integer idCliente) {
         return fichaService.getAllFichasByidCliente(idCliente);
     }
 
-    @PostMapping(value="/",consumes="application/json")
+    @PostMapping(consumes="application/json")
     @Operation(summary="Agrega una ficha", description="Debe enviar el objeto ficha a ingresar", tags= {"Fichas"})
     private @ResponseBody void addFicha(@RequestBody Ficha ficha) {
         fichaService.addFicha(ficha);
@@ -65,11 +52,12 @@ public class FichaController {
 
     @DeleteMapping(path="/{id}", produces="application/json")
     @Operation(summary="Borra una ficha de la Base de Datos", description="Debe enviar el id del ficha a borrar", tags= {"Fichas"})
-    private @ResponseBody void deleteFicha(@PathVariable("id") Integer id) {
+    private ResponseEntity<Void> deleteFicha(@PathVariable("id") Integer id) {
         fichaService.deleteFicha(id);
+        return ResponseEntity.noContent().build();
     }
 
-    @PutMapping(value="/", consumes="application/json")
+    @PutMapping(consumes="application/json")
     @Operation(summary="Permite modificar un ficha", description="Debe enviar el objeto a modificar, no se puede modificar el id", tags= {"Fichas"})
     private @ResponseBody Ficha updateFicha(@RequestBody Ficha ficha) {
         return fichaService.updateFicha(ficha);
