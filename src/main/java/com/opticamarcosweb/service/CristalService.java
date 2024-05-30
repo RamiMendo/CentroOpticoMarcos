c46a061913ba2e4a4c857ec9423a6ae9ac21fe75
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-30 15:34:50 -0300

    Agrega metodo de busqueda de cristales paginado en CristalService

--- src/main/java/com/opticamarcosweb/service/CristalService.java
+++ src/main/java/com/opticamarcosweb/service/CristalService.java
@@ -3,7 +3,11 @@ package com.opticamarcosweb.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.opticamarcosweb.exceptions.EntidadException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.opticamarcosweb.model.Cristal;
@@ -17,6 +21,10 @@ public class CristalService {
 
 	public List<Cristal> getAllCristales(){
 		return cristalRepository.findAll();
+	}
+
+	public Page<Cristal> getAllCristalesPaginado(Pageable pageable){
+		return cristalRepository.findAll(pageable);
 	}
 	
 	public Optional<Cristal> getCristalById(Integer IDCristal) {
