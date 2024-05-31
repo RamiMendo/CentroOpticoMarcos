2a65e706459728927a4c2fb22cf7ea356b248670
Ramiro Mendoza <ramamendoza@gmail.com>
2024-05-31 16:49:24 -0300

    Creacion del metodo getSaldoCliente en el Repositorio Cliente

--- src/main/java/com/opticamarcosweb/repository/ClienteRepository.java
+++ src/main/java/com/opticamarcosweb/repository/ClienteRepository.java
@@ -1,8 +1,18 @@
 package com.opticamarcosweb.repository;
 
+import com.opticamarcosweb.model.Ficha;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.opticamarcosweb.model.Cliente;
+
+import java.util.List;
+
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente,Integer>{}
+public interface ClienteRepository extends JpaRepository<Cliente,Integer>{
+
+    @Query(value = "SELECT SUM(Saldo) FROM Fichas WHERE id_cliente = :id ",nativeQuery = true)
+    Double getSaldoCliente(@Param("id") Integer id);
+}
