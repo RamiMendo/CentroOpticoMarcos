66219ba393126364a7693934abd5bf4b587906f3
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-01 17:42:54 -0300

    Creacion de la excepcion personalizada padre

---
+++ src/main/java/com/opticamarcos/exceptions/CustomException.java
@@ -0,0 +1,17 @@
+package com.opticamarcos.exceptions;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public abstract class CustomException extends Exception{
+
+    private String message;
+    private HttpStatus httpStatus;
+
+    public CustomException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
