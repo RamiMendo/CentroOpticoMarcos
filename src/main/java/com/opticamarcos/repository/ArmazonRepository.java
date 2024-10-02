2f9dd9417f6a56709a43526490dd475b2eadb49c
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-02 11:24:21 -0300

    Agrega funcionalidades al repositorio de armazon

---
+++ src/main/java/com/opticamarcos/repository/ArmazonRepository.java
@@ -0,0 +1,19 @@
+package com.opticamarcos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.opticamarcos.model.Armazon;
+
+import java.util.List;
+
+@Repository
+public interface ArmazonRepository extends JpaRepository<Armazon, Integer>, ArmazonRepositoryCustom {
+
+    //CUSTOM DERIVED QUERY
+    List<Armazon> findArmazonesByPrecio(Integer currentPage, Double regularDesde, Double regularHasta);
+
+//    List<Armazon> findByTieneDescuento(Integer currentPage, Boolean tieneDescuento);
+//    List<Armazon> findArmazonesByPreciosDescuento(Integer currentPage, Double descuentoDesde, Double descuentoHasta);
+//    List<Armazon> findArmazonesByPreciosTarjeta(Integer currentPage, Double tarjetaDesde, Double tarjetaHasta);
+}
