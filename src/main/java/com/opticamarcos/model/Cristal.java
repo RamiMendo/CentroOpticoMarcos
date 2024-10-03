34582c391e0fc4b114169e38d0e69185f9675fd1
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-03 10:13:15 -0300

    Añade notacion para ignorar el campo medidas en cristales

---
+++ src/main/java/com/opticamarcos/model/Cristal.java
@@ -0,0 +1,35 @@
+package com.opticamarcos.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="cristales")
+@JsonIgnoreProperties(value = "medidas")
+public class Cristal {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="id_cristal")
+	private Integer idCristal;
+
+	@Column(nullable = false, length = 50)
+	private String nombre;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cristal")
+	@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+	private List<Medida> medidas = new ArrayList<>();
+}
