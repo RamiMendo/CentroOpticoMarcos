b0f8ec24cca3724eccb0f9b242726986cb58720e
Ramiro Mendoza <ramamendoza@gmail.com>
2024-10-03 09:52:57 -0300

    Agrega metodos al repositorio de clientes

---
+++ src/main/java/com/opticamarcos/repository/ClienteRepository.java
@@ -0,0 +1,20 @@
+package com.opticamarcos.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.opticamarcos.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente,Integer>{
+
+    @Query(value = "SELECT SUM(Saldo) FROM Fichas WHERE id_cliente = :id ",nativeQuery = true)
+    Double getSaldoCliente(@Param("id") Integer id);
+
+    @Query(value = "SELECT C.* FROM Clientes C LEFT JOIN Fichas F ON C.id_cliente = F.id_cliente WHERE F.id_cliente is null;", nativeQuery = true)
+    Page<Cliente> findAllNoTieneFichas(Pageable pageable);
+}
