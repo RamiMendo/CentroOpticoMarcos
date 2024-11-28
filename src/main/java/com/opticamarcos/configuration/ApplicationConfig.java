4d43012bc0131d72d05babf33367fceff1e26d8f
Ramiro Mendoza <ramamendoza@gmail.com>
2024-11-28 16:35:14 -0300

    Habilita Proxy en clase Configuration

--- src/main/java/com/opticamarcos/configuration/ApplicationConfig.java
+++ src/main/java/com/opticamarcos/configuration/ApplicationConfig.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
     @Bean
