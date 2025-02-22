18a187468501c147c21ae7676610bf4ec54c0ac3
Ramiro Mendoza <ramamendoza@gmail.com>
2025-02-22 17:14:45 -0300

    Agrego metodo SecurityFilterChain en configuracion SecurityConfig

--- src/main/java/com/opticamarcos/configuration/SecurityConfig.java
+++ src/main/java/com/opticamarcos/configuration/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.opticamarcos.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+            return httpSecurity
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .httpBasic(Customizer.withDefaults())
+                    .formLogin(Customizer.withDefaults())
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .build();
+    }
+
 }
