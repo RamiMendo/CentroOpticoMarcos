9b88ee6a5011191b9afafb13550fd13ac613d700
Ramiro Mendoza <ramamendoza@gmail.com>
2025-02-23 17:40:46 -0300

    Cambio de tipo de retorno de List a Page en interface FichaRepositoryCustom

--- src/main/java/com/opticamarcos/repository/FichaRepositoryCustom.java
+++ src/main/java/com/opticamarcos/repository/FichaRepositoryCustom.java
@@ -1,12 +1,14 @@
 package com.opticamarcos.repository;
 
 import com.opticamarcos.model.entity.Ficha;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface FichaRepositoryCustom {
 
-    List<Ficha> getFichasByFiltro(LocalDate fechaDesde, LocalDate fechaHasta, Double totalDesde, Double totalHasta, Integer estaSeniado, Integer estaPago);
+    Page<Ficha> getFichasByFiltro(Pageable pageable, LocalDate fechaDesde, LocalDate fechaHasta, Double totalDesde, Double totalHasta, Integer estaSeniado, Integer estaPago);
 
 }
